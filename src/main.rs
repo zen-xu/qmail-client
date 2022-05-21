fd56aeb2e1e6d68a2085d2265dd3d313fe76aa58
ZhengYu, Xu <zen-xu@outlook.com>
2022-05-21 22:23:53 +0800

    impl tui for search

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod client;
+mod search;
 
 use std::fs;
 use std::{fmt::Display, str::FromStr};
@@ -6,11 +7,8 @@ use std::{fmt::Display, str::FromStr};
 use chrono::format::{parse, ParseError, Parsed, StrftimeItems};
 use chrono::{Datelike, FixedOffset, NaiveDate, TimeZone};
 use clap::{Parser, Subcommand};
-use owo_colors::{OwoColorize, Style};
 use serde::Serialize;
 use serde_json::Value;
-use tabled::object::{Object, Rows};
-use tabled::{object::Columns, Format, Modify, Table, Tabled};
 
 #[derive(Parser, Debug)]
 #[clap(author, version)]
@@ -90,21 +88,14 @@ impl Display for DateTime {
     }
 }
 
-#[derive(Tabled, Serialize)]
+#[derive(Serialize)]
 struct SearchResult {
-    #[tabled(rename = "id")]
     id: u32,
-    #[tabled(rename = "Subject")]
     subject: String,
-    #[tabled(rename = "From")]
     from: String,
-    #[tabled(rename = "To")]
     to: String,
-    #[tabled(rename = "CC")]
     cc: String,
-    #[tabled(rename = "Date")]
     date: String,
-    #[tabled(rename = "Attachments")]
     attachments: String,
 }
 
@@ -152,83 +143,30 @@ fn main() {
             mail_box,
             json,
         } => {
-            let mail_box = client.get(&mail_box).unwrap();
-            let mails = mail_box
-                .filter(&subject_query, start_datetime.0)
-                .end_date(end_datetime.0)
-                .regex(regex)
-                .reverse(reserve)
-                .fetch();
-            let mails = mails
-                .into_iter()
-                .map(SearchResult::from_mail)
-                .collect::<Vec<_>>();
-
             if json {
+                let mail_box = client.get(&mail_box).unwrap();
+                let mails = mail_box
+                    .filter(&subject_query, start_datetime.0)
+                    .end_date(end_datetime.0)
+                    .regex(regex)
+                    .reverse(reserve)
+                    .fetch();
+                let mails = mails
+                    .into_iter()
+                    .map(SearchResult::from_mail)
+                    .collect::<Vec<_>>();
                 println!("{}", serde_json::to_string(&mails).unwrap());
             } else {
-                println!(
-                    "{}",
-                    Table::new(mails)
-                        // column headers
-                        .with(Modify::new(Columns::single(1).not(Rows::new(1..))).with(
-                            Format::new(|s| s.green().style(Style::new().bold()).to_string())
-                        ))
-                        .with(Modify::new(Columns::single(2).not(Rows::new(1..))).with(
-                            Format::new(|s| s.yellow().style(Style::new().bold()).to_string())
-                        ))
-                        .with(Modify::new(Columns::single(3).not(Rows::new(1..))).with(
-                            Format::new(|s| s.cyan().style(Style::new().bold()).to_string())
-                        ))
-                        .with(Modify::new(Columns::single(4).not(Rows::new(1..))).with(
-                            Format::new(|s| s.blue().style(Style::new().bold()).to_string())
-                        ))
-                        .with(Modify::new(Columns::single(5).not(Rows::new(1..))).with(
-                            Format::new(|s| s.magenta().style(Style::new().bold()).to_string())
-                        ))
-                        .with(Modify::new(Columns::single(6).not(Rows::new(1..))).with(
-                            Format::new(|s| {
-                                s.bright_black().style(Style::new().bold()).to_string()
-                            })
-                        ))
-                        // rows
-                        .with(
-                            Modify::new(Columns::single(1).not(Rows::single(0)))
-                                .with(Format::new(|s| s.green().to_string()))
-                        )
-                        .with(Modify::new(Columns::single(2).not(Rows::single(0))).with(
-                            Format::new(|s| {
-                                s.split('\n')
-                                    .map(|s| s.yellow().to_string())
-                                    .collect::<Vec<_>>()
-                                    .join("\n")
-                            })
-                        ))
-                        .with(Modify::new(Columns::single(3).not(Rows::single(0))).with(
-                            Format::new(|s| {
-                                s.split('\n')
-                                    .map(|s| s.cyan().to_string())
-                                    .collect::<Vec<_>>()
-                                    .join("\n")
-                            })
-                        ))
-                        .with(Modify::new(Columns::single(4).not(Rows::single(0))).with(
-                            Format::new(|s| {
-                                s.split('\n')
-                                    .map(|s| s.blue().to_string())
-                                    .collect::<Vec<_>>()
-                                    .join("\n")
-                            })
-                        ))
-                        .with(
-                            Modify::new(Columns::single(5).not(Rows::single(0)))
-                                .with(Format::new(|s| { s.magenta().to_string() }))
-                        )
-                        .with(
-                            Modify::new(Columns::single(6).not(Rows::single(0)))
-                                .with(Format::new(|s| s.bright_black().to_string()))
-                        )
-                );
+                search::run(
+                    client,
+                    subject_query,
+                    start_datetime.0,
+                    end_datetime.0,
+                    regex,
+                    reserve,
+                    mail_box,
+                )
+                .unwrap();
             }
         }
         Commands::Folders => todo!(),
---
+++ src/search.rs
@@ -0,0 +1,212 @@
+use std::{error::Error, io, vec};
+
+use chrono::FixedOffset;
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Layout},
+    style::{Color, Modifier, Style},
+    widgets::{Block, Borders, Cell, Row, Table, TableState},
+    Frame, Terminal,
+};
+
+use crate::client::{Client, Mail};
+
+struct App {
+    state: TableState,
+    client: Client,
+    subject_query: String,
+    start_datetime: chrono::DateTime<FixedOffset>,
+    end_datetime: chrono::DateTime<FixedOffset>,
+    regex: bool,
+    reserve: bool,
+    mail_box: String,
+    mails: Vec<Mail>,
+}
+
+impl App {
+    pub fn new(
+        client: Client,
+        subject_query: String,
+        start_datetime: chrono::DateTime<FixedOffset>,
+        end_datetime: chrono::DateTime<FixedOffset>,
+        regex: bool,
+        reserve: bool,
+        mail_box: String,
+    ) -> App {
+        App {
+            state: TableState::default(),
+            client,
+            subject_query,
+            start_datetime,
+            end_datetime,
+            regex,
+            reserve,
+            mail_box,
+            mails: vec![],
+        }
+    }
+
+    pub fn refresh(&mut self) {
+        let mail_box = self.client.get(&self.mail_box).unwrap();
+        self.mails = mail_box
+            .filter(&self.subject_query, self.start_datetime)
+            .end_date(self.end_datetime)
+            .regex(self.regex)
+            .reverse(self.reserve)
+            .fetch();
+    }
+
+    pub fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.mails.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.mails.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+}
+
+pub fn run(
+    client: Client,
+    subject_query: String,
+    start_datetime: chrono::DateTime<FixedOffset>,
+    end_datetime: chrono::DateTime<FixedOffset>,
+    regex: bool,
+    reserve: bool,
+    mail_box: String,
+) -> Result<(), Box<dyn Error>> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // create app and run it
+    let mut app = App::new(
+        client,
+        subject_query,
+        start_datetime,
+        end_datetime,
+        regex,
+        reserve,
+        mail_box,
+    );
+    app.refresh();
+    let res = run_app(&mut terminal, app);
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{:?}", err)
+    }
+
+    Ok(())
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| ui(f, &mut app))?;
+
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                KeyCode::Char('q') => return Ok(()),
+                KeyCode::Down => app.next(),
+                KeyCode::Up => app.previous(),
+                _ => {}
+            }
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
+    let rects = Layout::default()
+        .constraints([Constraint::Percentage(100)].as_ref())
+        .split(f.size());
+
+    let selected_style = Style::default().add_modifier(Modifier::REVERSED);
+    let normal_style = Style::default().bg(Color::Blue);
+    let header_cells = ["id", "Subject", "From", "To", "CC", "Date", "Attachments"]
+        .iter()
+        .map(|h| Cell::from(*h).style(Style::default().fg(Color::Red)));
+    let header = Row::new(header_cells)
+        .style(normal_style)
+        .height(1)
+        .bottom_margin(1);
+    let rows = app.mails.iter().map(|item| {
+        let mail_fields = [
+            item.uid.to_string(),
+            item.subject.to_string(),
+            item.from.to_string(),
+            item.to.join("\n"),
+            item.cc.join("\n"),
+            item.internal_date.format("%Y-%m-%dT%H:%M:%S").to_string(),
+            item.attachments
+                .iter()
+                .map(|a| a.name.clone())
+                .collect::<Vec<String>>()
+                .join("\n"),
+        ];
+
+        let height = mail_fields
+            .iter()
+            .map(|content| content.chars().filter(|c| *c == '\n').count())
+            .max()
+            .unwrap_or(0)
+            + 1;
+        let cells = mail_fields.iter().enumerate().map(|(idx, c)| {
+            let style = match idx {
+                0 => Style::default().fg(Color::DarkGray),
+                _ => Style::default(),
+            };
+            Cell::from(c.clone()).style(style)
+        });
+        Row::new(cells).height(height as u16).bottom_margin(1)
+    });
+    let t = Table::new(rows)
+        .header(header)
+        .block(Block::default().borders(Borders::ALL).title("Mails"))
+        .highlight_style(selected_style)
+        .highlight_symbol(">> ")
+        .widths(&[
+            Constraint::Length(5),
+            Constraint::Percentage(20),
+            Constraint::Percentage(20),
+            Constraint::Percentage(20),
+            Constraint::Percentage(10),
+            Constraint::Length(20),
+            Constraint::Percentage(20),
+        ]);
+    f.render_stateful_widget(t, rects[0], &mut app.state);
+}
